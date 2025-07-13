07792f04a3f77ff70350d76ccb5b6c9a9ec47983
Ajeesh Joshy <jishcem@gmail.com>
2025-07-13 12:34:25 +0530

    Otpimize docker images

--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -1,8 +1,7 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
-import { UserDocument } from './models/user.schema';
-import { currentUser } from '@app/common';
+import { currentUser, UserDocument } from '@app/common';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 
 @Controller('users')
